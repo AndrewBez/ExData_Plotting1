2fa8472570c15f513a22b8ad395a8e054ee86c01
AndrewBez <ABezuhlyy@central-insurance.com>
2019-09-27 14:10:01 -0400

    plot 1

--- plot1.r
+++ plot1.r
@@ -1,1 +1,18 @@
+#download and unzip the dataset
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+whole_set <- read.table(unz(temp, "household_power_consumption.txt"),header=TRUE, sep = ";",stringsAsFactors = FALSE)
+unlink(temp)
 
+#subset the dataset to keep given two days of data only 
+two_days_set <- whole_set[whole_set$Date %in% c("2/1/2007","2/2/2007"),]
+
+#prepare the vector to plot
+data_to_plot <- as.numeric(two_days_set$Global_active_power[!two_days_set$Global_active_power=="?"])
+
+#plot the histogram
+hist(data_to_plot, breaks=seq(0,6,l=13), col = "red",main = "Global Active Power",xlab = "Global Active Power (kilowatts)")
+
+#output the plot to .png file
+dev.copy(device=png,filename="plot1.png")
+dev.off()
