dc54b252fc01378b4c77f5772895e105c6224bd7
AndrewBez <ABezuhlyy@central-insurance.com>
2019-09-27 15:40:07 -0400

    plot 2

---
+++ plot2.r
@@ -0,0 +1,19 @@
+#download and unzip the dataset
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+whole_set <- read.table(unz(temp, "household_power_consumption.txt"),header=TRUE, sep = ";",stringsAsFactors = FALSE)
+unlink(temp)
+
+#subset the dataset to keep given two days of data only 
+two_days_set <- whole_set[whole_set$Date %in% c("2/1/2007","2/2/2007"),]
+
+#prepare the time data
+timedata_raw <- paste(two_days_set$Date,two_days_set$Time)
+timedata <- strptime(timedata_raw, format = "%m/%d/%Y %H:%M:%S")
+
+#plot the graph
+plot(timedata,two_days_set$Global_active_power, type = "l",xlab="",ylab = "Global Active Power(kilowatts)")
+
+#output the plot to .png file
+dev.copy(device=png,filename="plot2.png")
+dev.off()
